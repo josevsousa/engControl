be3fa0c4cf0da17b307ee99f5d03717a87d6a81f
josevsousa <jose.vicente.de.sousa@gmail.com>
2023-10-10 18:29:59 -0300

    estruturas de pastas ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './pages/login/login.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: InicioComponent,
+        title: 'inicio'
+    },
+    {
+        path: 'inicio',
+        component: LoginComponent,
+        title: 'login'
+    }    
+];
---
+++ src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
+})
+export class ToolbarComponent {
+
+}
