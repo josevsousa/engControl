c20948d779d44f23f6d278ad2eca2d8333b569f0
josevsousa <jose.vicente.de.sousa@gmail.com>
2023-10-19 09:45:44 -0300

    angular-8 ok

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,29 +2,64 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/services/login.service';
+import { MatDialog, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatDialogModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
+
 export class DashboardComponent {
-
-
   constructor(
     private router: Router,
-    private auth: LoginService
+    private auth: LoginService,
+    public dialog: MatDialog
     ){
     console.log('estou na dashboard');
   }
+  
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(DialogAnimationsExampleDialog, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+
 
   logout(){
-    const confirmation = confirm('Deseja mesmo sair?');
-    if(confirmation){
-      this.auth.desconectarGoogle();
-    }
+    console.log("desconectando do google")
+    // const confirmation = confirm('Deseja mesmo sair?');
+    // if(confirmation){
+    //   this.auth.desconectarGoogle();
+    // }
   }
+
+}
+
+// --------------------------------------------------------
+
+@Component({
+  selector: 'dialog-animations-example-dialog',
+  templateUrl: 'dialog.html',
+  styleUrls: ['dialog.scss'],
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+})
+export class DialogAnimationsExampleDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DialogAnimationsExampleDialog>,
+    private auth: LoginService,
+    ) {}
+
+    logout(){
+      setTimeout(()=>{
+        this.auth.desconectarGoogle();
+      },550)
+    }
 
 }
